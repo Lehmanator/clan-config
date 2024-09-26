d614121bf48f8ffa9c106bbd9f1f6abdb6b36805
clan-tool <clan@example.com>
2024-09-26 11:54:14 -0400

    fix: machines.fw - disable buildHost/targetHost [WIP]

--- machines/fw/configuration.nix
+++ machines/fw/configuration.nix
@@ -8,11 +8,22 @@
 
   # Locale service discovery and mDNS
   services.avahi.enable = true;
-
+  nixpkgs.hostPlatform = "x86_64-linux";
   disko.devices.disk.main.device = "/dev/disk/by-id/nvme-WD_BLACK_SN770_2TB_22382X803513";
   clan.core = {
-    machineDescription = "Framework Laptop 13";
-    machineIcon = ./logo.png;
+    deployment.requireExplicitUpdate = true;
+    networking = {
+
+      # The build SSH node where nixos-rebuild will be executed.
+      # format: user@host:port&SSH_OPTION=SSH_VALUE
+      # examples:
+      # - machine.example.com
+      # - user@machine2.example.com
+      # - root@example.com:2222&IdentityFile=/path/to/private/key
+      # buildHost = "root@wyse.local";
+
+      # The target SSH node for deployment.
+      # targetHost = "sam@fw.local";
+    };
   };
-  nixpkgs.hostPlatform = "x86_64-linux";
 }
