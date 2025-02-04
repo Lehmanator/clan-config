c351d249d7c1270e96efd01e1a7e9d56dcc8de15
Sam Lehman <github@samlehman.dev>
2025-02-04 18:36:10 -0500

    feat(machines.fw): init nixos-facter
    - feat(machines.fw): init facter.json
    - refactor(machines.fw): remove unnecessary imports
    - refactor(machines.fw): remove unused imports
    - refactor(machines.fw): remove hardware-configuration.nix

--- machines/fw/configuration.nix
+++ machines/fw/configuration.nix
@@ -1,8 +1,3 @@
-{inputs, ...}: {
-  imports = [
-    ./hardware-configuration.nix
-    ./disko.nix
-  ];
-  clan.localsend.ipv4Addr = "192.168.57.2/24";
+{...}: {
   clan.core.deployment.requireExplicitUpdate = true;
 }
